8f9a715d9ae00a610f5bf8f82dbcbe48a97fb3bc
Icebob <mereg.norbert@gmail.com>
2020-09-08 15:39:03 +0200

    add eslint+prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,31 @@
 module.exports = {
-    root: true,
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es6": true,
-        "jquery": false,
-        "jest": true,
-        "jasmine": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-		"ecmaVersion": "2018"        
-    },
-    "rules": {
-        "indent": [
-            "warn",
-            "tab"
-        ],
-        "quotes": [
-            "warn",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-var": [
-            "error"
-        ],
-        "no-console": [
-            "off"
-        ],
-        "no-unused-vars": [
-            "warn"
-        ]
-    }
+	env: {
+		node: true,
+		commonjs: true,
+		es6: true,
+		jquery: false,
+		jest: true,
+		jasmine: true
+	},
+	extends: ["eslint:recommended", "plugin:security/recommended", "plugin:prettier/recommended"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2018
+	},
+	plugins: ["node", "promise", "security"],
+	rules: {
+		quotes: ["warn", "double"],
+		semi: ["error", "always"],
+		"no-var": ["error"],
+		"no-console": ["warn"],
+		"no-unused-vars": ["warn"],
+		"no-trailing-spaces": ["error"],
+		"security/detect-object-injection": ["off"],
+		"security/detect-non-literal-require": ["off"],
+		"security/detect-non-literal-fs-filename": ["off"],
+		"no-process-exit": ["off"],
+		"node/no-unpublished-require": 0,
+		"require-atomic-updates": 0,
+		"object-curly-spacing": ["warn", "always"]
+	}
 };
